4d6a656f4fd53124f393e86e61cb728b5efb0d27
lingyajun <lingyajun@tutanota.com>
2019-07-15 16:54:14 +0800

    math.pi

---
+++ basic-part-1/4.py
@@ -0,0 +1,17 @@
+a = 100 % 3
+b = 100 / 3
+c = 100 // 3
+
+print(a,' - ', b,' - ', c)
+
+import math
+
+r1 = 10
+s1 = math.pi * r1**2
+
+print(s1)
+
+from math import pi
+r = float(input('input radius of circle:'))
+s = pi * r**2
+print('the area of the circle with radius ', str(r), ' is: ', str(s))
