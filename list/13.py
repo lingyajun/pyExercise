b0340356d8c0ad1d06576c0faf9aca45f19ec4e8
lingyajun <lingyajun@tutanota.com>
2019-07-24 22:51:07 +0800

    random/shuffle ; 3D array

---
+++ list/13.py
@@ -0,0 +1,4 @@
+data = [ [ ['*' for col in range(3)] for col in range(4) ] for row in range(6) ]
+
+print(data)
+
---
+++ list/15.py
@@ -0,0 +1,12 @@
+#import random.shuffle
+from random import shuffle
+
+nums = [1,2,3,4,5,6]
+shuffle(nums)
+
+print(nums)
+
+colors = ['red','green','blue']
+shuffle(colors)
+
+print(colors)
