637a7e832eb482ed44ad8632ba57d6f14d67c4d8
lingyajun <lingyajun@tutanota.com>
2019-07-26 10:24:23 +0800

    convert list to string; list index

---
+++ list/20.py
@@ -0,0 +1,4 @@
+data = [11,22,33,34]
+
+for i, v in enumerate(data):
+    print(i, v)
---
+++ list/21.py
@@ -0,0 +1,11 @@
+data = [11,22,33,34]
+data2 = ['aa','zz',]
+
+s = ''.join(data2)
+print(s)
+
+s0 = ''
+for a in data:
+    s0 += str(a)
+
+print(s0)
