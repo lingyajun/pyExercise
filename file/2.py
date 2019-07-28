0f9a619403dcea6cd88cd75b6734d67a00059418
lingyajun <lingyajun@tutanota.com>
2019-07-28 10:27:01 +0800

    file read head lines

--- file/2.py
+++ file/2.py
@@ -9,3 +9,11 @@ dir1 = 'test.txt'
 n = 5
 print(read_first_lines(dir1, n))
 
+from itertools import islice
+def read_first_lines2(dir1, n):
+    with open(dir1, 'r') as f:
+        for line in islice(f, n):
+            print(line)
+
+read_first_lines2(dir1, n)
+
